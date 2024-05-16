408e73119352b3bd8b41f1daf28ca26b6b89bce3
Hugo Grandine <hugog_3@hotmail.com>
2024-05-16 12:24:40 -0300

    criacao do hr-worker

---
+++ hr-worker/src/main/java/com/hgrandine/hrworker/HrWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.hgrandine.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkerApplication.class, args);
+	}
+
+}
